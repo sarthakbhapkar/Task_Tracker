d60e013df63cd452186e730aee6e8eebe6f8cfe6
sarthakbhapkar <65077678+sarthakbhapkar@users.noreply.github.com>
2024-01-31 10:52:03 +0530

    Create ResourceNotFoundException.java

---
+++ ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+// ResourceNotFoundException.java
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
